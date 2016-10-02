2c999f98ba9532b68cdffb4efa09bc4a62d95ee0
SDkie <kumar.s@media.net>
2016-10-02 22:29:08 +0530

    Added unit test for reading User Data

--- data/employee_test.go
+++ data/employee_test.go
@@ -39,7 +39,6 @@ func testingSetup() {
 }
 
 var _ = Describe("Creating Employee", func() {
-
 	BeforeEach(func() {
 		testingSetup()
 		Expect(db.GetDb().Create(&dept).Error).NotTo(HaveOccurred())
@@ -48,16 +47,67 @@ var _ = Describe("Creating Employee", func() {
 
 	Context("Valid User Creation", func() {
 		It("User Should Create Successfully", func() {
-			_, err := data.CreateEmployee(emp.EName, emp.Job, emp.Mgr, emp.Salary, emp.DeptNo)
+			newEmp, err := data.CreateEmployee(emp.EName, emp.Job, emp.Mgr, emp.Salary, emp.DeptNo)
 			Expect(err).NotTo(HaveOccurred())
+			Expect(newEmp.EName).Should(Equal(emp.EName))
+			Expect(newEmp.Job).Should(Equal(emp.Job))
+			Expect(newEmp.Mgr).Should(Equal(emp.Mgr))
+			Expect(newEmp.Salary).Should(Equal(emp.Salary))
+			Expect(newEmp.DeptNo).Should(Equal(emp.DeptNo))
 		})
 
 	})
 
-	Context("Invalid User Creation", func() {
+	Context("User Creation with invalid DeptNo", func() {
 		It("User Creation should fail", func() {
 			_, err := data.CreateEmployee(emp.EName, emp.Job, emp.Mgr, emp.Salary, -1)
 			Expect(err).Should(HaveOccurred())
+		})
+	})
+
+	AfterEach(func() {
+		db.Close()
+	})
+})
+
+var _ = Describe("Reading Employee Data", func() {
+	BeforeEach(func() {
+		testingSetup()
+		Expect(db.GetDb().Create(&dept).Error).NotTo(HaveOccurred())
+		emp.DeptNo = dept.DeptNo
+		Expect(db.GetDb().Create(&emp).Error).NotTo(HaveOccurred())
+		emp.EName += "1"
+		emp.EmpNo = 0
+		emp.DeptNo = dept.DeptNo
+		Expect(db.GetDb().Create(&emp).Error).NotTo(HaveOccurred())
+	})
+
+	Context("Reading User List", func() {
+		It("Should Get All DB Users", func() {
+			emps, err := data.ListOfAllEmployees()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(emps).Should(HaveLen(2))
+		})
+	})
+
+	Context("Reading Single User", func() {
+		It("Should Get Single DB User", func() {
+			newEmp, err := data.GetEmployeeByEmpNo(emp.EmpNo)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(newEmp.EmpNo).Should(Equal(emp.EmpNo))
+			Expect(newEmp.EName).Should(Equal(emp.EName))
+			Expect(newEmp.Job).Should(Equal(emp.Job))
+			Expect(newEmp.Mgr).Should(Equal(emp.Mgr))
+			Expect(newEmp.Salary).Should(Equal(emp.Salary))
+			Expect(newEmp.DeptNo).Should(Equal(emp.DeptNo))
+		})
+	})
+
+	Context("Reading User List by DName", func() {
+		It("Should Get All DB Users", func() {
+			emps, err := data.ListOfAllEmployeesByDname(dept.Dname)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(emps).Should(HaveLen(2))
 		})
 	})
 
