584081a1960e1d788873a0af5ca38ff1964e05e0
SDkie <kumar.s@media.net>
2016-10-04 02:35:51 +0530

    Added Testcases for UpdateEmployee

--- gq/mutation_test.go
+++ gq/mutation_test.go
@@ -184,3 +184,105 @@ var _ = Describe("deleteEmployee", func() {
 		db.Close()
 	})
 })
+
+var _ = Describe("updateEmployee", func() {
+
+	type updateEmployeeResponse struct {
+		Data struct {
+			Employee data.Employee `json:"updateEmployee"`
+		} `json:"data"`
+
+		Errors []gqError `json:"errors"`
+	}
+
+	BeforeEach(func() {
+		setup()
+		// Creating DeptNo 1
+		Expect(db.GetDb().Create(&dept).Error).NotTo(HaveOccurred())
+
+		// Creating DeptNo 2
+		emp.DeptNo = dept.DeptNo
+		dept.Dname = "NewDept"
+		dept.DeptNo = 0
+		Expect(db.GetDb().Create(&dept).Error).NotTo(HaveOccurred())
+
+		// Creating User
+		Expect(db.GetDb().Create(&emp).Error).NotTo(HaveOccurred())
+	})
+
+	Context("Sending valid graphql query", func() {
+
+		It("should be updated successfully", func() {
+			query := `
+		mutation {
+			updateEmployee(EMPNO:%d ENAME:"%s", JOB:"%s", MGR:%d, SALARY:%f, DEPTNO:%d){
+				ENAME,
+				JOB,
+				MGR,
+				SALARY,
+				DEPT {
+					DEPTNO,
+					DNAME,
+					LOC
+				}
+			}
+		}`
+
+			// Change Name, Job and Dept
+			emp.EName = "QWERTY_2"
+			emp.Job = "NewJob"
+			emp.DeptNo = dept.DeptNo
+			query = fmt.Sprintf(query, emp.EmpNo, emp.EName, emp.Job, emp.Mgr, emp.Salary, dept.DeptNo)
+
+			resp := sendRequest(query)
+			response := new(updateEmployeeResponse)
+			err := json.Unmarshal(resp.Body.Bytes(), response)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(response.Errors).Should(HaveLen(0))
+			empDb := response.Data.Employee
+			Expect(empDb.EName).Should(Equal(emp.EName))
+			Expect(empDb.Job).Should(Equal(emp.Job))
+			Expect(empDb.Mgr).Should(Equal(emp.Mgr))
+			Expect(empDb.Salary).Should(Equal(emp.Salary))
+			Expect(empDb.Dept.Dname).Should(Equal(dept.Dname))
+			Expect(empDb.Dept.DeptNo).Should(Equal(dept.DeptNo))
+			Expect(empDb.Dept.Loc).Should(Equal(dept.Loc))
+
+			fmt.Println(resp.Body)
+		})
+	})
+
+	Context("Not Sending EmpNo", func() {
+
+		It("should fail", func() {
+			query := `
+		mutation {
+			updateEmployee(ENAME:"%s", JOB:"%s", MGR:%d, SALARY:%f, DEPTNO:%d){
+				ENAME,
+				JOB,
+				MGR,
+				SALARY,
+				DEPT {
+					DEPTNO,
+					DNAME,
+					LOC
+				}
+			}
+		}`
+
+			query = fmt.Sprintf(query, emp.EName, emp.Job, emp.Mgr, emp.Salary, -1)
+			resp := sendRequest(query)
+
+			response := new(updateEmployeeResponse)
+			err := json.Unmarshal(resp.Body.Bytes(), response)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(response.Errors).ShouldNot(HaveLen(0))
+		})
+	})
+
+	AfterEach(func() {
+		db.Close()
+	})
+})
