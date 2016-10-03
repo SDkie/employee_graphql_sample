8a30d49dc1edd77f04cefddeee40e33177ad19fb
SDkie <kumar.s@media.net>
2016-10-03 10:23:25 +0530

    added unit tests for getEmployee graph query

--- gq/employee_test.go
+++ gq/employee_test.go
@@ -154,3 +154,88 @@ var _ = Describe("createEmployee Graph Query", func() {
 		db.Close()
 	})
 })
+
+var _ = Describe("getEmployee Graph Query", func() {
+
+	type getEmployeeResponse struct {
+		Data struct {
+			CreateEmployee data.Employee `json:"getEmployee"`
+		} `json:"data"`
+
+		Errors []gqError `json:"errors"`
+	}
+
+	BeforeEach(func() {
+		testingSetup()
+		Expect(db.GetDb().Create(&dept).Error).NotTo(HaveOccurred())
+		emp.DeptNo = dept.DeptNo
+		Expect(db.GetDb().Create(&emp).Error).NotTo(HaveOccurred())
+	})
+
+	Context("Sending valid graphql query", func() {
+
+		It("We should get valid user response", func() {
+			query := `
+		query {
+			getEmployee(EMPNO:%d){
+				ENAME,
+				JOB,
+				MGR,
+				SALARY,
+				DEPT {
+					DEPTNO,
+					DNAME,
+					LOC
+				}
+			}
+		}`
+
+			query = fmt.Sprintf(query, emp.EmpNo)
+
+			resp := sendRequest(query)
+			response := new(getEmployeeResponse)
+			err := json.Unmarshal(resp.Body.Bytes(), response)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(response.Errors).Should(HaveLen(0))
+			Expect(response.Data.CreateEmployee.EName).Should(Equal(emp.EName))
+			Expect(response.Data.CreateEmployee.Job).Should(Equal(emp.Job))
+			Expect(response.Data.CreateEmployee.Mgr).Should(Equal(emp.Mgr))
+			Expect(response.Data.CreateEmployee.Salary).Should(Equal(emp.Salary))
+			Expect(response.Data.CreateEmployee.Dept.Dname).Should(Equal(dept.Dname))
+			Expect(response.Data.CreateEmployee.Dept.DeptNo).Should(Equal(dept.DeptNo))
+			Expect(response.Data.CreateEmployee.Dept.Loc).Should(Equal(dept.Loc))
+		})
+	})
+
+	Context("Sending Invalid EMPNO in query", func() {
+
+		It("Not sending EMPNO in request", func() {
+			query := `
+		query {
+			getEmployee{
+				ENAME,
+				JOB,
+				MGR,
+				SALARY,
+				DEPT {
+					DEPTNO,
+					DNAME,
+					LOC
+				}
+			}
+		}`
+
+			resp := sendRequest(query)
+			response := new(getEmployeeResponse)
+			err := json.Unmarshal(resp.Body.Bytes(), response)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(response.Errors).ShouldNot(HaveLen(0))
+		})
+	})
+
+	AfterEach(func() {
+		db.Close()
+	})
+})
